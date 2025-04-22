de0dfa1e6b8637123d9b495e107f212c69125323
Faizankhan17623 <faizankhan901152@gmail.com>
2025-04-22 15:21:30 +0530

    13

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const DatabaseConnection = async ()=>{
-    mongoose.connect(process.env.DATABASE_URL)
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(()=>{
         console.log("The databse connection is been done".bgBlue)
     })
