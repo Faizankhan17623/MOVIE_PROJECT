00cac3970da43f2d5bbaa5ec5376acf929cefcd3
Faizankhan17623 <faizankhan901152@gmail.com>
2025-04-25 18:40:11 +0530

    3

--- backend/routes/Payment.js
+++ backend/routes/Payment.js
@@ -3,10 +3,10 @@ const route = express.Router()
 const {auth,IsUSER} = require('../middlewares/verification')
 const {MakePayment,Verifypayment,MakePdf} = require('../controllers/common/Payment')
 // DONE
-route.post("/Make-Payment",MakePayment)
-route.post("/Verify-Payment",Verifypayment)
+route.post("/Make-Payment",auth,IsUSER,MakePayment)
+route.post("/Verify-Payment",auth,IsUSER,Verifypayment)
 // This is the route that will make the pdf of that ticket
-route.get('/download/:ticketId',MakePdf)
+route.get('/download/:ticketId',auth,IsUSER,MakePdf)
 // DONE
 
 module.exports = route
