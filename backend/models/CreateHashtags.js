47547ccf6127f503d5f30c93e565f5dab281c7db
Faizankhan17623 <faizankhan901152@gmail.com>
2025-06-11 14:56:21 +0530

    This is the backend code

--- backend/models/CreateHashtags.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const CreatehashtagSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true
-    }
-})
-module.exports = mongoose.model("Hashtags",CreatehashtagSchema)
--- backend/models/subgenre.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const subGenereSchema =  new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-},{timestamps:true})
-module.exports = mongoose.model('SubGeneres',subGenereSchema)
