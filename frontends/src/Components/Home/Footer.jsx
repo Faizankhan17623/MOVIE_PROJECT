5f75676c9ce4fb6a200cc4c254e157c8f3c1a63d
Faizankhan17623 <faizankhan901152@gmail.com>
2025-06-11 17:34:55 +0530

    This is the fronteend commit

--- frontends/src/Components/Home/Footer.jsx
+++ frontends/src/Components/Home/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div>
       <div>
         {/* This is tthe firstt section and itt conttains tthee logo and all tthe ottheerr tthnigs */}
-        <div className=''>
+        <div className='slideerr'>
 
         </div>
         <div></div>
